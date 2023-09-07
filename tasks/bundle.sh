547da187ab32d2775a75652556c4457a86d42be3
Baldur Bjarnason <baldur.bjarnason@gmail.com>
2023-09-07 16:00:10 +0000

    Deno version of raggedy

--- tasks/bundle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-rm -rf dist
-
-./.bin/esbuild $* --bundle --outdir=dist --format=esm --splitting --minify --sourcemap
--- tasks/serve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-PORT=8888
-
-./.bin/esbuild --serve=$PORT --servedir=$PWD --keyfile=.bin/dev.key --certfile=.bin/dev.cert
-# The below might not be necessary
-# trap 'rm -rf .ignore' EXIT INT TERM HUP
