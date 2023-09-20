661649e9bb541338d216ef694954d80d5c49850b
Baldur Bjarnason <baldur.bjarnason@gmail.com>
2023-09-20 14:15:14 +0000

    Prettier console output

--- ci/pageWithCoverage.js
+++ ci/pageWithCoverage.js
@@ -5,6 +5,7 @@ export { expect } from "@playwright/test";
 import { randomUUID } from "node:crypto";
 
 // This new "test" can be used in multiple test files, and each of them will get the fixtures.
+// You need to use the import map to properly load modules and this means that you need to either use HTTP dependencies or rebase the vendored import map (like tasks/serve does)
 export const test = base.extend({
 	pageWithCoverage: async ({ page, browserName }, use) => {
 		if (browserName === "chromium") {
